6221c859e3e34c0d89e2a7aa8817ead8c1973f3f
zzq1314zll <zzqlovezll@gmail.com>
2021-07-06 09:53:06 +0800

    [ISSUE#1]Add cliuster list

---
+++ src/main/java/com/alibaba/nacos/cli/commands/cluster/NacosCluster.java
@@ -0,0 +1,34 @@
+package com.alibaba.nacos.cli.commands.cluster;
+
+import picocli.CommandLine;
+
+import static com.alibaba.nacos.cli.utils.HintUtils.CLUSTER;
+import static com.alibaba.nacos.cli.utils.HintUtils.CLUSTER_CONFIG;
+import static com.alibaba.nacos.cli.utils.HintUtils.DESCRIPTION_CLUSTER;
+import static com.alibaba.nacos.cli.utils.HintUtils.DESCRIPTION_HEADING;
+import static com.alibaba.nacos.cli.utils.HintUtils.HEADER_HEADING;
+import static com.alibaba.nacos.cli.utils.HintUtils.OPTION_LIST_HEADING;
+import static com.alibaba.nacos.cli.utils.HintUtils.PARAMETER_LIST_HEADING;
+import static com.alibaba.nacos.cli.utils.HintUtils.SORT_OPTIONS;
+import static com.alibaba.nacos.cli.utils.HintUtils.SYNOPSIS_HEADING;
+
+/**
+ * cluster subcommand.
+ *
+ * @author zzq
+ */
+@CommandLine.Command(name = CLUSTER, sortOptions = SORT_OPTIONS,
+        headerHeading = HEADER_HEADING, synopsisHeading = SYNOPSIS_HEADING,
+        descriptionHeading = DESCRIPTION_HEADING, parameterListHeading = PARAMETER_LIST_HEADING,
+        optionListHeading = OPTION_LIST_HEADING, header = CLUSTER_CONFIG, description = DESCRIPTION_CLUSTER,
+        subcommands = {CommandLine.HelpCommand.class, NacosClusterList.class})
+public class NacosCluster implements Runnable {
+    
+    @CommandLine.Spec
+    CommandLine.Model.CommandSpec spec;
+    
+    @Override
+    public void run() {
+        spec.commandLine().usage(System.err);
+    }
+}
